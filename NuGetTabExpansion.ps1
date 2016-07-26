5dde0f9ec2f4b7c4dd31785178dd99b5935b17f6
Alex Panov <alpaix@outlook.com>
2016-07-25 22:42:53 -0700

    Added restore options

--- NuGetTabExpansion.ps1
+++ NuGetTabExpansion.ps1
@@ -18,21 +18,21 @@ $NuGetCommands = @(
 
 # usage: NuGet restore [<solution> | <packages.config file> | <project.json> | <Microsoft Build project>] [options]
 $RestoreOptions = @(
-    '-RequireConsent'
-    '-Project2ProjectTimeOut'
-    '-PackagesDirectory'
-    '-SolutionDirectory'
-    '-MSBuildVersion'
-    '-Source +'
-    '-FallbackSource +'
-    '-NoCache'
-    '-DisableParallelProcessing'
-    '-PackageSaveMode'
-    '-Help'
-    '-Verbosity'
-    '-NonInteractive'
-    '-ConfigFile'
-    '-ForceEnglishOutput'
+    'RequireConsent'
+    'Project2ProjectTimeOut'
+    'PackagesDirectory'
+    'SolutionDirectory'
+    'MSBuildVersion'
+    'Source +'
+    'FallbackSource +'
+    'NoCache'
+    'DisableParallelProcessing'
+    'PackageSaveMode'
+    'Help'
+    'Verbosity'
+    'NonInteractive'
+    'ConfigFile'
+    'ForceEnglishOutput'
 )
 
 if (Test-Path Function:\TabExpansion) {
@@ -40,12 +40,19 @@ if (Test-Path Function:\TabExpansion) {
     Rename-Item Function:\TabExpansion TabExpansionNext
 }
 
-function ExpandRestoreOptions($tokens) {
-    $RestoreOptions
+function ExpandRestoreOptions([string[]]$usedOptions, $lastWord) {
+    if (-not $lastWord) {
+        return $RestoreOptions | ?{ $usedOptions -notcontains $_ } | %{ "-$_"}
+    }
+    if ($lastWord -like '-*') {
+        return ($RestoreOptions | ?{ $usedOptions -notcontains $_ } | %{ "-$_"}) -like "$lastWord*"
+    }
+
+    return 'xui'
 }
 
-$NuGetExePattern = '^[''"]?.*nuget[\.\d-]*(?:\.exe)?[''"]?\s+(\w+)?'
-$OptionsPattern = '\s+(-\w+)(\s+[^-][^\s]+)?'
+$NuGetExePattern = '^([''"]?).*nuget[\.\d-]*(?:\.exe)?\k<1>\s+'
+$OptionsPattern = '\s+-(\w+)(\s+[^-][^\s]+)?'
 
 function TabExpansion($line, $lastWord) {
 
@@ -55,35 +62,37 @@ function TabExpansion($line, $lastWord) {
         #return TabExpansionNext $line $lastWord
     }
 
-    $matchCmd = $matchNuGet.Matches.Groups[-1]
+    $line = $line.Substring($matchNuGet.Matches[0].Length)
 
-    if ($matchNuGet.Matches.Groups[0].Length -eq $line.Length) {
-
-        if (-not $matchCmd.Success) {
-            return $NuGetCommands
-        }
-
-        return $NuGetCommands -like "$($matchCmd.Value)*"
+    if (-not $line) {
+        return $NuGetCommands
     }
 
-    $from = $matchCmd.Index + $matchCmd.Length
-    $args = $line.Substring($from)
-
-    if (-not $args) {
-        return $RestoreOptions
+    if ($line -eq $lastWord) {
+        return ($NuGetCommands -like "${lastWord}*") + @($lastWord) | select -Unique
     }
 
-    $opts = $args | sls -pattern $OptionsPattern -AllMatches | %{ $_.Matches } | %{ $_.Value }
-    return $opts
-<#
+    $matchOpts = $line | sls -pattern $OptionsPattern -AllMatches | %{ $_.Matches }
+    $opts = $matchOpts | %{ $_.Groups[1].Value }
+
+    if ($matchOpts) {
+       $pos = $line.Substring(0, $matchOpts[0].Index)
+    }
+    else {
+        $pos = $line
+    }
+
+    $matchCmd = $pos | sls -pattern '\w+' | %{ $_.Matches } | select -first 1
+
     switch ($matchCmd.Value) {
         'restore' {
-            return ExpandRestoreOptions $tokens[2..($tokens.Count-1)]
+            return ExpandRestoreOptions $opts $lastWord
          }
         Default {
             return TabExpansionNext $line $lastWord
         }
     }
-    return TabExpansionNext $line $lastWord
-#>
+
+    #return TabExpansionNext $line $lastWord
+    return 'pizdets'
 }
